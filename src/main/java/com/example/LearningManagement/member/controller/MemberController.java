7745f275f4ec60966e58646b616972a9234e963c
Kanggyeonggu <kku1031@naver.com>
2023-07-14 21:40:28 +0900

    thymeleaf 문법을 통한 동일 ID로 가입 실패 처리

--- src/main/java/com/example/LearningManagement/member/controller/MemberController.java
+++ src/main/java/com/example/LearningManagement/member/controller/MemberController.java
@@ -1,16 +1,14 @@
 package com.example.LearningManagement.member.controller;
 
-import com.example.LearningManagement.member.entity.Member;
 import com.example.LearningManagement.member.model.MemberInput;
 import com.example.LearningManagement.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.time.LocalDateTime;
 
 @Controller
 @RequiredArgsConstructor
@@ -27,10 +25,12 @@ public class MemberController {
     //response server -> web
     @PostMapping("/member/register")
     //스프링이 request와 response에 대해서 의존성 주입함.
-    public String registerSubmit(HttpServletRequest request, HttpServletResponse response
+    //Model : 클라이언트에게 데이터를 내리기 위해 사용하는 Interface
+    public String registerSubmit(Model model, HttpServletRequest request
             , MemberInput parameter) {
 
         boolean result = memberService.register(parameter);
+        model.addAttribute("result", result);
 
         return "member/register_complete";
     }
--- src/main/java/com/example/LearningManagement/member/service/MemberServiceImpl.java
+++ src/main/java/com/example/LearningManagement/member/service/MemberServiceImpl.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,8 +15,15 @@ public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
+    //회원가입
     @Override
     public boolean register(MemberInput parameter) {
+
+        Optional<Member> optionalMember = memberRepository.findById(parameter.getUserId());
+        if (optionalMember.isPresent()) {
+            //현재 userId에 해당하는 데이터 존재
+            return false;
+        }
 
         Member member = new Member();
 
